52ec7208b9c516f109df6947f66535848589cd2a
chiharakenta <chiharakenta0519@gmail.com>
2020-02-10 14:14:03 +0900

    add user posts relation

--- db/migrate/20200210034744_change_column_to_not_null.rb
+++ db/migrate/20200210034744_change_column_to_not_null.rb
@@ -1,8 +1,5 @@
 class ChangeColumnToNotNull < ActiveRecord::Migration[6.0]
-  def up
-    change_column_null :users, :google_id, :string, null: false
-  end
-  def down
-    change_column :users, :google_id, :string
+  def change
+    change_column_null :users, :google_id, null: false
   end
 end
